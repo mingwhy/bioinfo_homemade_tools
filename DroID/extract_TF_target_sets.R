b74bb3b2dd0b7e5a13b95ac106949beee578b27a
mingwhy <70790127+mingwhy@users.noreply.github.com>
2021-09-08 08:30:31 -0700

    Update extract_TF_target_sets.R

--- DroID/extract_TF_target_sets.R
+++ DroID/extract_TF_target_sets.R
@@ -1,4 +1,5 @@
-
+# check out:
+# https://github.com/mingwhy/bioinfo_homemade_tools/tree/main/DroID
 options(stringsAsFactors = F)
 library(org.Dm.eg.db)
 
@@ -17,10 +18,17 @@ sum(is.na(df$TF_symbol)) #225
 sum(is.na(df$target_symbol)) #2245
 df=df[!is.na(df$TF_symbol) & !is.na(df$target_symbol),]
 dim(df) #154995      5
+
+sum(df$TF_symbol=='dsx') #2
+sum(df$target_symbol=='dsx') #17
+df[df$TF_symbol=='dsx' | df$target_symbol=='dsx',]
+
 # filter TF based on target gene size
 x=sort(table(df$TF_symbol))
 length(x) #146 TF
 sum(x>=5) #87 TFs
 df1=df[df$TF_symbol %in% names(which(x>=5)),]
 head(df1)
+sum(df1$target_symbol=='dsx') #17
+
 saveRDS(df1,'TF-target_size5.rds')
