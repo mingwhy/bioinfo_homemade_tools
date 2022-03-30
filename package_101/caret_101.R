eec2037e346961828ad852c7cfbf7c93dc89f77c
mingwhy <70790127+mingwhy@users.noreply.github.com>
2022-03-29 18:04:12 -0700

    Add files via upload

--- package_101/caret_101.R
+++ package_101/caret_101.R
@@ -1,8 +1,28 @@
+##########################################
+# svm with caret (https://rpubs.com/uky994/593668)
+library(tidyverse)
+library(caret)
+
+# Load the data
+data("PimaIndiansDiabetes2", package = "mlbench")
+pima.data <- na.omit(PimaIndiansDiabetes2)
+# Inspect the data
+sample_n(pima.data, 3)
+
+# Set up Repeated k-fold Cross Validation
+train_control <- trainControl(method="repeatedcv", number=5, repeats=1)
+# Fit the model (SVM linear classifier)
+svm1 <- train(diabetes ~., data = pima.data, method = "svmLinear", trControl = train_control,  preProcess = c("center","scale"))
+#View the model
+svm1
+
+
+##########################################
 #Example for svm feature selection in R
 #https://stackoverflow.com/questions/17529537/example-for-svm-feature-selection-in-r
 #http://topepo.github.io/caret/recursive-feature-elimination.html
 #http://topepo.github.io/caret/recursive-feature-elimination.html#rfe
-library(caret)
+
 data(BloodBrain, package="caret")
 logBBB
 x <- scale(bbbDescr[,-nearZeroVar(bbbDescr)])
