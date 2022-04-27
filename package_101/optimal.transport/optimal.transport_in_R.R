49f69f2fdf75971a0d109624aa825bf88b5d28f3
mingwhy <70790127+mingwhy@users.noreply.github.com>
2022-04-26 21:55:46 -0700

    Add files via upload

---
+++ package_101/optimal.transport/optimal.transport_in_R.R
@@ -0,0 +1,31 @@
+#https://www.r-bloggers.com/2017/07/matching-optimal-transport-and-statistical-tests/
+#https://freakonometrics.hypotheses.org/50958
+
+#remotes::install_github("pierrejacob/winference") #for cost_matrix_Lp 
+# didn't work, change to python (https://pythonot.github.io/auto_examples/plot_Intro_OT.html)
+set.seed(13)
+npoints <- 25
+mu1 <- c(1,1)
+mu2 <- c(0,2)
+Sigma1 <- diag(1, 2, 2)
+Sigma2 <- diag(1, 2, 2)
+Sigma2[2,1] <- Sigma2[1,2] <- -0.5
+Sigma1 <- 0.4 * Sigma1
+Sigma2 <- 0.4 *Sigma2
+
+library(mnormt)
+X1 <- rmnorm(npoints, mean = mu1, Sigma1)
+X2 <- rmnorm(npoints, mean = mu2, Sigma2)
+plot(X1[,1], X1[,2], ,col="blue")
+points(X2[,1], X2[,2], col = "red")
+
+
+ground_p <- 2
+p <- 1
+w1 <- rep(1/npoints, npoints)
+w2 <- rep(1/npoints, npoints)
+
+library(transport)
+library(winference)
+C <- cost_matrix_Lp(t(X1), t(X2), ground_p)
+a <- transport(w1, w2, costm = C^p, method = "shortsimplex")
