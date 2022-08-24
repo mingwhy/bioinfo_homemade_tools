13bca93b67c80a5f08db46e56f9f17e0b008ba4f
mingwhy <70790127+mingwhy@users.noreply.github.com>
2022-08-24 09:12:47 -0700

    Add files via upload

--- package_101/python_run_MMD_batch.py
+++ package_101/python_run_MMD_batch.py
@@ -43,10 +43,10 @@ for tc in all_tc:
 	# change `#if adata_sample.shape[0] > 50:` in decibel.py into `adata_sample.shape[0] >= 40:` as my tc contain>=50cells
 
 	ages=sce.obs['age'].unique()
-	for i in ages[0:1]:
-		for j in ages[1:2]:
+	for age1 in ages[0:1]:
+		for age2 in ages[1:2]:
 			print(i,j,tc)
-			test=sce[sce.obs['age'].isin([i,j])]			
+			test=sce[sce.obs['age'].isin([age1,age2])]			
 			distances, p_values = scmmd.compute_mmd_contrast(
 			    adata=test, # [Cells, Genes] object
 			    representation='X_pca', # representation to use, "X" or key in `adata.obsm`.
