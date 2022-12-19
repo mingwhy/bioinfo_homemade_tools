5012f999348153467cc9b9110e6728727cfaeb68
mingwhy <70790127+mingwhy@users.noreply.github.com>
2022-12-19 11:22:30 -0800

    Update biomart_transcription_start_end_site.R

--- used.cases/biomaRt_usage/biomart_transcription_start_end_site.R
+++ used.cases/biomaRt_usage/biomart_transcription_start_end_site.R
@@ -1,3 +1,8 @@
+
+## use biomaRt to get gene chr info 
+# https://github.com/mingwhy/bioinfo_homemade_tools/tree/main/used.cases/biomaRt_usage
+# add gene_biotype info: https://support.bioconductor.org/p/62441/
+if(F){ #run once
 #https://stackoverflow.com/questions/13012210/find-transcription-start-sites-with-biomart
 library(biomaRt)
 packageVersion("biomaRt") #‘2.50.3’
@@ -16,11 +21,16 @@ attributes = listAttributes(ensembl)
 attributes[grep('dmel',attributes$name),]
 grep('transcript',attributes$name,value=TRUE,ignore.case = TRUE)
 
-t2g<-getBM(attributes=c('ensembl_gene_id','chromosome_name','start_position','end_position',
-                        'ensembl_transcript_id','flybase_transcript_id',
-                        "transcript_start", "transcript_end",
-                        'transcription_start_site', "transcript_length","transcript_count"), mart = ensembl)
+t2g<-getBM(attributes=c('ensembl_gene_id',"gene_biotype",
+                        "ensembl_transcript_id","transcript_start","transcript_end",
+                        #"ensembl_exon_id","exon_chrom_start","exon_chrom_end",
+                        "strand","chromosome_name","transcript_biotype", 
+                        'chromosome_name','start_position','end_position',
+                        'flybase_transcript_id',"transcript_length","transcript_count"), mart = ensembl)
 dim(t2g) #41209    11
 head(t2g)
 saveRDS(t2g,'t2g_chr.coord.rds')
-
+}
+t2g=readRDS('t2g_chr.coord.rds')
+# one gene has multiple rows as muliple transcripts
+df.gene.length=t2g[!duplicated(t2g$ensembl_gene_id),]
